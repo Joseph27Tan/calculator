ed0a9f1c957ed4ef0c0c38e96c66f62a9928f786
Joseph27Tan <joseph27tan2004@gmail.com>
2025-03-16 15:13:59 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+
+(script.js) 
+let display=
+document.getElementById("display");
+
+// Function to append values to the display function
+appendToDisplay(value)
+( display.value+= value);
+
+// Function to clear the display function clear Display ()
+( display.value)
+
+// Function to evaluate the expression function calculate()try
+{ display.value 
+    eval(display.value);}  { alert ("invalid expressioni")} 
+    
+
+
+
+
+
+
+
+
+
+
+
+
